04273709b6eca74edcce405ab66c596e3227e013
phonkala <pasi.honkala@actamedia.fi>
2025-01-23 16:12:26 +0200

    Modified spell Metamorphosis

--- spells/livescripts/src/metamorphosis.ts
+++ spells/livescripts/src/metamorphosis.ts
@@ -1,5 +1,9 @@
 export function Main(events: TSEvents) {
 
+
+
+    // Convert runes to death runes.
+
     events.Spell.OnCast(UTAG("tswow-module-public-tests.spells", "spell.metamorphosis"), (spell) => {
 
         const caster = ToPlayer(spell.GetCaster());
@@ -11,6 +15,31 @@ export function Main(events: TSEvents) {
         }
 
         caster.ResyncRunes();
+
+    });
+
+
+
+    // Remove Metamorphosis aura OnLogin to avoid issues with runes. Can probably be fixed better way later with more core edits.
+
+    events.Player.OnLogin((player, firstLogin) => {
+
+        player.RemoveAura(UTAG("tswow-module-public-tests.spells", "spell.metamorphosis"));
+
+    });
+
+
+
+    // Remove the complementary auras when Metamorphosis is removed.
+
+    events.Spell.OnRemove(UTAG("tswow-module-public-tests.spells", "spell.metamorphosis"), (effect, application, type) => {
+
+        const player = ToPlayer(effect.GetCaster());
+        if ( !player ) return;
+
+        player.RemoveAura(UTAG("tswow-module-public-tests.spells", "spell.metamorphosis.modify-movement-speed-and-armor"));
+        player.RemoveAura(UTAG("tswow-module-public-tests.spells", "spell.metamorphosis.modify-runes-and-autoattack"));
+        player.RemoveAura(UTAG("tswow-module-public-tests.spells", "spell.metamorphosis.modify-haste"));
 
     });
 
